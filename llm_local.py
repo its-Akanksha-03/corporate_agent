178880f64a57d0e35f20c3984f1b6d979036e846
Kushalp2004 <kushalphiremath2004@gmail.com>
2025-08-15 01:53:43 +0530

    feat: Restructured project files into a new folder hierarchy

--- llm_local.py
+++
@@ -1,9 +0,0 @@
-import subprocess
-
-def query_ollama(prompt, model="mistral"):
-    result = subprocess.run(
-        ["ollama", "run", model],
-        input=prompt.encode(),
-        stdout=subprocess.PIPE
-    )
-    return result.stdout.decode()
